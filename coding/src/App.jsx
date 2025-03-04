fd5d09f48d6d6172320684ab34f97c270d30ff01
Niruth Ananth <niruthananth03@gmail.com>
2025-03-04 20:21:03 +0530

    React Router installed

--- coding/src/App.jsx
+++ coding/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import {Routes,Route} from "react-router-dom"
+import Home from './pages/Home'
 
 function App() {
   
   return (
-    <div>
-      <h1 className='text-center'>Hello world</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
   )
 }
 
